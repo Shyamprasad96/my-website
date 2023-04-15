5d5f939e7fe4e7fef92d7758be1e32a0104f85c2
Shyamprasad96 <syanamad@asu.edu>
2023-04-15 02:40:21 -0700

    About Page Carousels

--- src/Navigationbar.js
+++ src/Navigationbar.js
@@ -1,17 +1,18 @@
 import {Nav,Navbar,NavLink} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import "./Navigationbar.css"
 
 const Navigationbar=()=>{
     return(
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+        <Navbar className="navbar" collapseOnSelect expand="sm" >
             <Navbar.Toggle aria-controls="navbarScroll" data-bs-target="#NavbarScroll"/>
             <Navbar.Collapse id="NavbarScroll">
-                <Nav>
-                    <NavLink eventKey="1" as={Link} to="/">Home</NavLink>
-                    <NavLink eventKey="2" as={Link} to="/about">About</NavLink>
-                    <NavLink eventKey="3" as={Link} to="/code">Code</NavLink>
-                    <NavLink eventKey="4" as={Link} to="/personal">Personal</NavLink>
-                    <NavLink eventKey="5" as={Link} to="/work">Work</NavLink>
+                <Nav className="imageWrapper">
+                    <NavLink  className="lb1" eventKey="1" as={Link} to="/"><h5>Home</h5></NavLink>
+                    <NavLink className="lb" eventKey="2" as={Link} to="/about"><h5>About</h5></NavLink>
+                    <NavLink className="lb" eventKey="3" as={Link} to="/code"><h5>Code</h5></NavLink>
+                    <NavLink className="lb" eventKey="4" as={Link} to="/personal"><h5>Personal</h5></NavLink>
+                    <NavLink className="lb" eventKey="5" as={Link} to="/work"><h5>Work</h5></NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,15 @@
+import "./Home.css";
+import Image from 'react-bootstrap/Image';
 const Home=()=>{
+// eslint-disable-next-line 
     return(
-        <center><h1>Home Page</h1></center>
+    <div className="myimage">
+        <p>Shyam Prasad</p>
+        <br/>
+        <br/>
+        <span>I'm a Student</span>
+     <Image className="image" src="../Shyam.jpg" fluid />
+    </div>
     )
 
 }
