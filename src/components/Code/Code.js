d40d3175c538bac4b1759206ce0a677e4d8a5bfd
Shyamprasad96 <syanamad@asu.edu>
2023-04-15 22:11:21 -0700

    Code_page_Table

--- src/components/Code/Code.js
+++ src/components/Code/Code.js
@@ -1,6 +1,170 @@
+import './Code.css';
+import 'bulma/css/bulma.min.css';
 const Code=()=>{
     return(
-        <center><h1>Code Page</h1></center>
+        <div className="code-page">
+    <div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <table class="table table-bordered is-bordered is-fullwidth is-rounded table-hover footable">
+        <thead>
+          <tr>
+          
+            <th><div class="text-center table-text">Leetcode</div></th>
+            <th data-hide="phone,tablet"><div class="sortable text-start table-text" >Problem </div></th>
+            <th><div class="text-center table-text">Frequency</div></th>
+            <th data-hide="phone"><div class="text-center table-text">Tag</div></th>
+            <th><div class="text-center table-text">Code</div></th>
+            <th data-hide="phone"><div class="text-center table-text">Link</div></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text " href="https://leetcode.com/problems/contains-duplicate/">Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="45" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-success"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-dark is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/">  Longest Substring Without Repeating Characters </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="95" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-success"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="25" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-success"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-success"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="95" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="38" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-success"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="50" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-danger"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-danger"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="94" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="45" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+          <tr>
+          
+            <td ><div>217</div></td>
+            <td class="text-start"><a target="_blank" class="table-text" href="https://leetcode.com/problems/contains-duplicate/"> Contains Duplicate </a></td>
+            <td class="text-center"><div class="freq"><progress class="progress is-dark is-small" value="65" max="100"></progress></div></td>
+            <td  class="diff-col"><div class="flex-container-row"><button class="button is-rounded is-small is-light table-button is-warning"><b>Array</b></button></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Code</a></div></td>
+            <td ><div class="flex-container-row"><a target="_blank"  class="button is-dark is-small is-rounded is-outlined is-hover table-button" href="https://youtu.be/NgBoMJy386M"> Python</a></div></td>  
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="5" class="text-center">@ShyamPrasad </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </div>
+</div>
+        </div>
     )
 
 }
