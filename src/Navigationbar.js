0a95a36280b47d5a3942dadd7742327afc8576dd
Shyamprasad96 <syanamad@asu.edu>
2023-04-19 13:27:57 -0700

    Code before uploading to domain

--- src/Navigationbar.js
+++ src/Navigationbar.js
@@ -11,7 +11,7 @@ const Navigationbar=()=>{
                     <NavLink  className="lb1" eventKey="1" as={Link} to="/"><h5>Home</h5></NavLink>
                     <NavLink className="lb" eventKey="2" as={Link} to="/about"><h5>About</h5></NavLink>
                     <NavLink className="lb" eventKey="3" as={Link} to="/code"><h5>Code</h5></NavLink>
-                    <NavLink className="lb" eventKey="4" as={Link} to="/personal"><h5>Personal</h5></NavLink>
+                    <NavLink className="lb" eventKey="4" as={Link} to="/personal"><h5>Programs</h5></NavLink>
                     <NavLink className="lb" eventKey="5" as={Link} to="/work"><h5>Work</h5></NavLink>
                 </Nav>
             </Navbar.Collapse>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,18 @@ import Image from 'react-bootstrap/Image';
 const Home=()=>{
 // eslint-disable-next-line 
     return(
+    <div className="homepage">
+    <div className="home">
+    <p class="home-text"><span className="home-para">DailyLeetCode website is my personal blog, Where I am sharing my knowledge in python Programming, Data Structures and Algorithms to the students. Anyone who are intrested to learn the leetcode and Algorithms, It's the place you are looking for. This courses are free of cost. </span></p>
+    </div>
     <div className="myimage">
  <p className="ok">Shyam Prasad</p><br/><br/>
  <p className="span">I am a Student</p>
      <Image className="image" src="../Shyam.jpg" fluid />
-     <div className="hide"> Shyam Prasad</div>
     </div>
+    
+    </div>
+    
     )
 
 }
